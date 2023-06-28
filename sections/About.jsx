82d5dbb43d72a3e45f15c08bfb1f1c708f77a131
anoderb <64400505+anoderb@users.noreply.github.com>
2023-06-28 08:46:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ sections/About.jsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Ethernal" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Ethernal Space</span> is a new space in the Crypto world, where you can wade through vast spaces on various networks from layer 1 layer 2 or others, you can experience the ease of transactions on {' '}
+        <span className="font-extrabold text-white">
+          Ethernal Space
+        </span>{' '}
+        because this is the innovation{' '}
+        <span className="font-extrabold text-white">VR</span> devices you can
+        easily explore the metaverse world you want, turn your dreams into
+        reality. Let's{' '}
+        <span className="font-extrabold text-white">Ethernal Space</span> invites you to explore by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
